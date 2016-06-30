a8ecbb6e6b5790661312cf2563dbd1d5cfd844dc
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-06-29 23:06:04 -0500

    Operators and Operands

---
+++ app.js
@@ -0,0 +1,40 @@
+// Operators and Operands
+/************************
+Operand	Operator	Operand
+  100	     +	      50
+************************/
+
+var x = 5;
+var y = 2;
+var z = x + y;
+document.getElementById("demo").innerHTML = z;
+
+var x = 5;
+var y = 2;
+var z = x - y;
+document.getElementById("demo1").innerHTML = z;
+
+var x = 5;
+var y = 2;
+var z = x * y;
+document.getElementById("demo2").innerHTML = z;
+
+var x = 5;
+var y = 2;
+var z = x / y;
+document.getElementById("demo3").innerHTML = z;
+
+var x = 5;
+var y = 2;
+var z = x % y;
+document.getElementById("demo4").innerHTML = z;
+
+var x = 5;
+x++;
+var z = x;
+document.getElementById("demo5").innerHTML = z;
+
+var x = 5;
+x--;
+var z = x;
+document.getElementById("demo6").innerHTML = z;
